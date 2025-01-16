a56da5e08e63dcdf28508e21b365a58edd697c35
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2025-01-16 14:50:42 -0400

    fixed books handouts,init chat

--- src/app/pages/classroom/classroom.component.ts
+++ src/app/pages/classroom/classroom.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentNavComponent } from "../../shared/components/student-nav/student-nav.component";
+import { ChatModalComponent } from '../../features/chat/components/chat-modal/chat-modal.component';
 
 @Component({
   selector: 'app-classroom',
   standalone: true,
-  imports: [RouterOutlet, StudentNavComponent],
-  template: `
-    <div class="classroom-container">
-      <app-student-nav></app-student-nav>
-      <main class="classroom-content">
-        <router-outlet></router-outlet>
-      </main>
-    </div>
-  `,
+  imports: [RouterOutlet, StudentNavComponent,ChatModalComponent],
+  templateUrl: './classroom.component.html',
   styleUrls: ['./classroom.component.scss']
 })
 export class ClassroomComponent {}
