c35e4de7bcaf66245a279f69544b20c0eca57ca7
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2025-01-26 18:19:16 -0400

    banner

--- src/app/pages/ecommerce/features/banner/banner.component.ts
+++ src/app/pages/ecommerce/features/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-banner',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
+  openWhatsApp() {
+    const message = encodeURIComponent('Olá! Gostaria de saber mais sobre os cursos.');
+    const whatsappUrl = `https://wa.me/${environment.tel}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  }
 
+  scrollToFaq(event: Event) {
+    event.preventDefault();
+    const faqSection = document.getElementById('faq');
+    if (faqSection) {
+      faqSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
